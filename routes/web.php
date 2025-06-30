d3be27fa00ac494d760c27d95313b5769fa6a124
ilhmstxr <ilhambintanghuh30332@gmail.com>
2025-06-30 12:54:36 +0700

    mod: ngerapihin login

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\AbsensiController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KompetisiController;
 use App\Http\Controllers\LabBookingController;
 use App\Http\Controllers\LabController;
@@ -22,3 +23,16 @@ Route::resource('LabBooking', LabBookingController::class);
 Route::resource('Lab', LabController::class);
 Route::resource('Perwalian', PerwalianController::class);
 Route::resource('Research', ResearchController::class);
+
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::prefix('dashboard')->middleware('auth')->group(function () {
+   
+});
