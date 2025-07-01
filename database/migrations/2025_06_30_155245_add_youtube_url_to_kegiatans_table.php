d660e36eac1d93734d508ec3b2a71b144597bea8
viviovilala <hauranabila2244@gmail.com>
2025-07-01 13:26:39 +0700

    url yt

--- database/migrations/2025_06_30_155245_add_youtube_url_to_kegiatans_table.php
+++ database/migrations/2025_06_30_155245_add_youtube_url_to_kegiatans_table.php
@@ -8,9 +8,6 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('kegiatans', function (Blueprint $table) {
-            // Menambahkan kolom youtube_url.
-            // Gunakan nullable() agar kolom ini tidak wajib diisi.
-            // Gunakan after('tempat') untuk menempatkannya setelah kolom 'tempat'
             $table->string('youtube_url')->nullable()->after('tempat');
         });
     }
@@ -18,7 +15,6 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('kegiatans', function (Blueprint $table) {
-            // Saat rollback, kolom youtube_url akan dihapus
             $table->dropColumn('youtube_url');
         });
     }
