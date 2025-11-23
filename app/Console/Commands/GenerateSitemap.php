07753e42d1e85fd7ee305567faa5934335aba0eb
Ragil Hidayatulloh <ragilhidayah1990@gmail.com>
2025-11-24 02:20:45 +0700

    coba seo 2

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -3,7 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemap extends Command
 {
@@ -28,9 +29,81 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        // Ganti URL dengan URL website Anda yang sebenarnya
-        SitemapGenerator::create('https://isslab.web.id')
-            ->writeToFile(public_path('sitemap.xml'));
+        $sitemap = Sitemap::create();
+
+        // Halaman Home
+        $sitemap->add(
+            Url::create('/')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(1.0)
+        );
+
+        // Halaman Profile
+        $sitemap->add(
+            Url::create('/Profile')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(0.9)
+        );
+
+        // Halaman Kegiatan
+        $sitemap->add(
+            Url::create('/Kegiatan')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.9)
+        );
+
+        // Halaman Research
+        $sitemap->add(
+            Url::create('/Research')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.9)
+        );
+
+        // Halaman Portofolio
+        $sitemap->add(
+            Url::create('/Portofolio')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.9)
+        );
+
+        // Halaman SOP
+        $sitemap->add(
+            Url::create('/Sop')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(0.7)
+        );
+
+        // Halaman Lab Booking
+        $sitemap->add(
+            Url::create('/LabBooking')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(0.7)
+        );
+
+        // Halaman Komunitas 1
+        $sitemap->add(
+            Url::create('/komunitas/1')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.8)
+        );
+
+        // Halaman Komunitas 2
+        $sitemap->add(
+            Url::create('/komunitas/2')
+                ->setLastModificationDate(now())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.8)
+        );
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
 
         $this->info('Sitemap generated successfully.');
         
